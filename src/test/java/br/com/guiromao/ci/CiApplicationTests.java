77591235b3705761670a8cb2cf797d4d63b202ea
guiromao87 <guilhermersimoes87@gmail.com>
2024-07-15 15:04:59 -0300

    commit inicial

---
+++ src/test/java/br/com/guiromao/ci/CiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiromao.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
